e8d5d600a45b4a486bf2935951040c6d08e28ed7
Talha Riaz <muhammadtalha1005@gmail.com>
2024-06-03 00:19:03 +0500

    well do

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
             , sessionExpiration: response.data.user.sessionExpiration
           }));
           localStorage.setItem("login", false)
-          router.push("#login")
+          router.push("/")
         }
       }
     } catch (error) {
